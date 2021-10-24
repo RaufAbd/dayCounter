bb7626830d03f055bc36984417e756f41c0b0ae4
RaufAbd <89908436+RaufAbd@users.noreply.github.com>
2021-10-24 23:27:26 +0400

    Update dayCounter.js

--- dayCounter.js
+++ dayCounter.js
@@ -1,7 +1,6 @@
 let date = new Date()
 
 let count = +prompt('please enter the number of days');
-// let dayList = ['Bazar günü', 'Bazar ertəsi', 'Çərşənbə axşamı', 'Çərşənbə', 'Cümə axşamı', 'Cümə', 'Şənbə']
 let dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 let sum = (date.getDay() + count) % 7; 
 
