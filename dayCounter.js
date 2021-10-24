1e5d575a546b299a5a4d08710895ba7ab209a4f4
RaufAbd <89908436+RaufAbd@users.noreply.github.com>
2021-10-25 02:01:41 +0400

    Update dayCounter.js

--- dayCounter.js
+++ dayCounter.js
@@ -9,12 +9,10 @@ let dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
 let sum = (date.getDay() + +count) % 7;
 
 let resultDate = new Date(date.setDate(date.getDate() + +count));
-let newDay = resultDate.getDate();
-let newMonth = resultDate.getMonth() + 1;
-let newYear = resultDate.getFullYear();
+let newDate = resultDate.getDate() + '/' + (resultDate.getMonth() + 1) + '/' + resultDate.getFullYear();
 
 if(sum < 0) {
     sum = dayList.length - Math.abs(sum);
-    alert(`${count} days ago it was ${dayList[sum]} and the date on the calendar was ${newDay}/${newMonth}/${newYear}`)
-} else alert(`after ${count} days it will be ${dayList[sum]} and the date on the calendar will be ${newDay}/${newMonth}/${newYear}`)
+    alert(`${count} days ago it was ${dayList[sum]} and the date on the calendar was ${newDate}`)
+} else alert(`after ${count} days it will be ${dayList[sum]} and the date on the calendar will be ${newDate}`)
 
