e97b2d78932c073b84286b0b24b5ff52ad26cf38
RaufAbd <89908436+RaufAbd@users.noreply.github.com>
2021-10-25 00:29:28 +0400

    Update dayCounter.js

--- dayCounter.js
+++ dayCounter.js
@@ -1,7 +1,14 @@
+do{
+    count = prompt('please enter the number of days');
+} while(!(isFinite(count) && count))
+
+const dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
 let date = new Date()
+let count;
+let sum = (date.getDay() + +count) % 7; 
 
-let count = +prompt('please enter the number of days');
-let dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-let sum = (date.getDay() + count) % 7; 
-
-alert(`after ${count} days it will be ${dayList[sum]}`)
+if(sum < 0) {
+    sum = dayList.length - Math.abs(sum);
+    alert(`${count} days ago it was ${dayList[sum]}`)
+} else alert(`after ${count} days it will be ${dayList[sum]}`)
