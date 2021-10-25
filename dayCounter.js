2fc7887195f2b9e25d4ca524427cb69fbf0d335b
RaufAbd <89908436+RaufAbd@users.noreply.github.com>
2021-10-25 18:14:30 +0400

    Update dayCounter.js

--- dayCounter.js
+++ dayCounter.js
@@ -1,12 +1,12 @@
+const dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
 let date = new Date()
 let input;
 
 do{
     input = prompt('please enter the number of days');
 } while(!(isFinite(input) && input))
-
-const dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 let num = Math.round(+input)
 let result = (date.getDay() + num) % 7;
@@ -17,5 +17,4 @@ if(num == 0) alert(`It's ${dayList[result]} today and the date is ${resultDate}`
 else if(num < 0) {
   (result != 0 && result < 0) ? result = dayList.length + result : result;
     alert(`${Math.abs(input)} days ago it was ${dayList[result]} and the date was ${resultDate}`)
-    }
-else alert(`After ${input} days it will be ${dayList[result]} and the date will be ${resultDate}`)
+} else alert(`After ${input} days it will be ${dayList[result]} and the date will be ${resultDate}`)
