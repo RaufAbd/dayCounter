5cc9efdcbf6714791e0af13322c4e8a50c6bb2f5
RaufAbd <89908436+RaufAbd@users.noreply.github.com>
2021-10-25 18:11:06 +0400

    Update dayCounter.js

--- dayCounter.js
+++ dayCounter.js
@@ -1,21 +1,21 @@
 let date = new Date()
-let count;
+let input;
 
 do{
-    count = prompt('please enter the number of days');
-} while(!(isFinite(count) && count))
+    input = prompt('please enter the number of days');
+} while(!(isFinite(input) && input))
 
-let dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-let monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+const dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-let sum = (date.getDay() + +count) % 7;
+let num = Math.round(+input)
+let result = (date.getDay() + num) % 7;
+let newDate = new Date(date.setDate(date.getDate() + num));
+let resultDate =  monthList[newDate.getMonth()] + " " + newDate.getDate() + ", " + newDate.getFullYear();
 
-let resultDate = new Date(date.setDate(date.getDate() + +count));
-let newDate =  monthList[resultDate.getMonth()] + " " + resultDate.getDate() + ", " + resultDate.getFullYear();
-
-if(sum < 0) {
-    sum = dayList.length + sum;
-    alert(`${Math.abs(count)} days ago it was ${dayList[sum]} and the date on the calendar was ${newDate}`)
-    } 
-else alert(`after ${count} days it will be ${dayList[sum]} and the date on the calendar will be ${newDate}`)
-
+if(num == 0) alert(`It's ${dayList[result]} today and the date is ${resultDate}`);
+else if(num < 0) {
+  (result != 0 && result < 0) ? result = dayList.length + result : result;
+    alert(`${Math.abs(input)} days ago it was ${dayList[result]} and the date was ${resultDate}`)
+    }
+else alert(`After ${input} days it will be ${dayList[result]} and the date will be ${resultDate}`)
