69f4707800940a85197710c2030c8d340340c687
RaufAbd <89908436+RaufAbd@users.noreply.github.com>
2021-10-25 09:20:32 +0400

    Update dayCounter.js

--- dayCounter.js
+++ dayCounter.js
@@ -5,14 +5,17 @@ do{
     count = prompt('please enter the number of days');
 } while(!(isFinite(count) && count))
 
-let dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+let dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'Nowember', 'December']
+
 let sum = (date.getDay() + +count) % 7;
 
 let resultDate = new Date(date.setDate(date.getDate() + +count));
-let newDate = resultDate.getDate() + '/' + (resultDate.getMonth() + 1) + '/' + resultDate.getFullYear();
+let newDate =  monthList[resultDate.getMonth()] + " " + resultDate.getDate() + " " + resultDate.getFullYear();
 
 if(sum < 0) {
     sum = dayList.length - Math.abs(sum);
-    alert(`${count} days ago it was ${dayList[sum]} and the date on the calendar was ${newDate}`)
-} else alert(`after ${count} days it will be ${dayList[sum]} and the date on the calendar will be ${newDate}`)
+    alert(`${Math.abs(count)} days ago it was ${dayList[sum]} and the date on the calendar was ${newDate}`)
+    } 
+else alert(`after ${count} days it will be ${dayList[sum]} and the date on the calendar will be ${newDate}`)
 
